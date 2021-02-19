5aa7e693a54662453bfb9caecca1f86a10f7c777
Sashabrava <sashabrava@yandex.by>
2021-02-19 09:36:30 +0100

    Activity checks connection to the server during onCreate and onSharedPrefereceChange

--- app/src/main/java/org/sashabrava/shopapp/MainActivity.java
+++ app/src/main/java/org/sashabrava/shopapp/MainActivity.java
@@ -1,12 +1,13 @@
 package org.sashabrava.shopapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,6 +16,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -25,6 +27,7 @@ import org.sashabrava.shopapp.server.ServerRequest;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private final SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener = (sharedPreferences, key) -> checkServerAlive(getApplicationContext());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +36,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
-            ServerRequest serverRequest = ServerRequest.getInstance(view.getContext());
-            try {
-                serverRequest.templateRequest(this,
-                        "api/check-alive",
-                        ServerRequest.class.getMethod("checkServerAlive", String.class),
-                        getApplicationContext(),
-                        this.getClass().getMethod("fabGreen", Object.class),
-                        this.getClass().getMethod("fabRed", String.class)
-                );
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-        });
+        fab.setOnClickListener(view -> checkServerAlive(view.getContext()));
+        fab.callOnClick();
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -59,6 +50,15 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
     }
 
     @Override
@@ -81,26 +81,39 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.action_settings) {
             Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
             MainActivity.this.startActivity(myIntent);
-            /*Snackbar.make(getWindow().getDecorView().findViewById(android.R.id.content), "You have clicked on settings", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-     public void fabGreen(Object object) {
-         FloatingActionButton fab = findViewById(R.id.fab);
-         fab.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+    public void fabGreen(Object object) {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
         Snackbar.make(fab, "Successfully received a response from Shop Server", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
-         ((NavigationView) findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_item).setEnabled(true);
+        ((NavigationView) findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_item).setEnabled(true);
     }
 
-     public void fabRed(String errorText) {
-         FloatingActionButton fab = findViewById(R.id.fab);
-         fab.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+    public void fabRed(String errorText) {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
         Snackbar.make(fab, errorText, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
-         ((NavigationView) findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_item).setEnabled(false);
+        ((NavigationView) findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_item).setEnabled(false);
+    }
+
+    public void checkServerAlive(Context context) {
+        ServerRequest serverRequest = ServerRequest.getInstance(context);
+        try {
+            serverRequest.templateRequest(this,
+                    "api/check-alive",
+                    ServerRequest.class.getMethod("checkServerAlive", String.class),
+                    getApplicationContext(),
+                    this.getClass().getMethod("fabGreen", Object.class),
+                    this.getClass().getMethod("fabRed", String.class)
+            );
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 }
